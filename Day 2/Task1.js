aa83eff9c62543ea482db5c3dda59172b4acb62d
thisen@gmail.com <thisen@gmail.com>
2021-12-02 22:26:12 +0100

    day 2, challange 1

---
+++ Day 2/Task1.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+
+let horizontalPosition = 0
+let depthPosition = 0
+let commands = {}
+
+
+// Add commands from input file to object using regex
+const convertCommandsToObject = (data) => {
+  const regex = new RegExp(/\s\n/gm)
+  parse = data.split(regex)
+
+  // Grab the direction
+  const directionRegex = new RegExp(/[a-z]+/gm)
+  // Grab the value
+  const valueRegex = new RegExp(/[0-9]+/gm)
+
+  // Add direction and value to commands object
+  for (let i = 0; i < parse.length; i++) {
+    commands[i] = {
+      'direction': parse[i].match(directionRegex),
+      'value': +parse[i].match(valueRegex),
+    }
+  }
+}
+
+// Calculate each command
+const calculatePosition = (data) => {
+  for (let i in data) {
+    // For each command, check the direction and add or subtract the value
+    switch (data[i].direction[0]) {
+      case 'forward' : {horizontalPosition += data[i].value} break
+      case 'down' : {depthPosition += data[i].value} break
+      case 'up': {depthPosition -= data[i].value} break
+    }
+  }
+}
+
+// Multiply the horizontal and depth position to get the final answer
+const finalPosition = (a, b) => {
+  return (console.log("Final position is:", a * b))
+}
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  // console.log(data)
+  convertCommandsToObject(data)
+  calculatePosition(commands)
+  console.log("Horizontal:", horizontalPosition, "Depth:", depthPosition)
+
+  finalPosition(horizontalPosition, depthPosition)
+})
